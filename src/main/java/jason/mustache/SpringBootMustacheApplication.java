3ceeef6f1a5dec820a77ed47decba3a1a11c6046
keumhwanmoon <hwanth85@gmail.com>
2016-08-28 21:39:03 +0900

    Initial commit

---
+++ src/main/java/jason/mustache/SpringBootMustacheApplication.java
@@ -0,0 +1,12 @@
+package jason.mustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMustacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMustacheApplication.class, args);
+	}
+}
